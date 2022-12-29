b1d1aad664551f9d7a969d81e0f733fdf0d931f1
ACOE DEVOPS <acoecolledecse@gmail.com>
2022-12-29 15:12:47 +0530

    first demo

---
+++ src/main/java/bsection/project/App.java
@@ -0,0 +1,13 @@
+package bsection.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DURGA PRASAD" );
+    }
+}
